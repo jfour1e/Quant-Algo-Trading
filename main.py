230ab06baa7dc4ae93235e0e4f34feebdc1f5fb5
James <jfourie@bu.edu>
2024-02-29 18:27:21 -0500

    Update main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+import yfinance as yf
+import pandas as pd
+import numpy as np
+
+tickers = ['PANW', 'SHOP', 'META', 'TSM', 'NET', 'DELL', 'ON', 'CRM', 'SONY', 'CRWD', 'AMAT']
+
+def get_data(stock):
+    return None 
